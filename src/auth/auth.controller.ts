28256ba04221448b1c646e1b910b548eadb61778
teamSynergySuite <team.synergisuite@gmail.com>
2024-10-17 22:48:45 +0500

    Update email completed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,30 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { PayloadDto } from './dto/payload.dto';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { userAlreadyExistGuard, userExistGuard } from 'src/user/user.guard';
-import { JwtGuard } from 'src/shared/auth.guard';
-import { Console } from 'console';
 import { Request } from 'express';
+import { JwtWithVerificationGuard } from 'src/shared/verification.guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @Post('/new')
-    @UseGuards(userAlreadyExistGuard)
-    async create(@Body() createUserDto: CreateUserDto) {
+  @Post('/new')
+  @UseGuards(userAlreadyExistGuard)
+  async create(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
   }
 
-    @Post("/login")
-    @UseGuards(userExistGuard)
-    async login (@Body() authPayload: PayloadDto) {
-        return await this.authService.validateSession(authPayload)
-    }
+  @Post("/login")
+  @UseGuards(userExistGuard)
+  async login (@Body() authPayload: PayloadDto) {
+    return await this.authService.validateSession(authPayload)
+  }
+
+  @Patch('update-email')
+  @UseGuards(JwtWithVerificationGuard)
+  updateEmail(@Req()reqObj: Request, @Body('userCode') userCode: number) {
+    return this.authService.verifyEmailCode(userCode, reqObj.user);
+  }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,15 +4,13 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { EmailModule } from 'src/mailer/email.module';
-import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from 'src/database/database.module';
-import { AuthModule } from 'src/auth/auth.module';
 import { RedisService } from 'src/redis/redis.service';
 import { RedisModule } from 'src/redis/redis.module';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), EmailModule, DatabaseModule, RedisModule ],
+  imports: [TypeOrmModule.forFeature([User]), EmailModule, DatabaseModule, RedisModule],
   controllers: [UserController],
   providers: [UserService, RedisService],
 })
